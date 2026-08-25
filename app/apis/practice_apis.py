93adcb3dc1940739259aaa379527d0c190b13fc9
kimi <nbhhh99@gmail.com>
2026-08-25 16:02:03 +0900

    Create practice_apis.py

---
+++ app/apis/practice_apis.py
@@ -0,0 +1,24 @@
+# app/apis/practice_apis.py
+user_list = [
+	{
+		"id": 1,
+		"name": "홍길동",
+		"age": 24,
+		"email": "gildong24@example.com",
+		"password": "Password1234!!"
+	},
+	{
+		"id": 2,
+		"name": "장문복",
+		"age": 21,
+		"email": "moonluck12@example.com",
+		"password": "Check1321!"
+	},
+	{
+		"id": 3,
+		"name": "임우진",
+		"age": 31,
+		"email": "limousine33@example.com",
+		"password": "lwsPAssword12@"
+	}
+]
